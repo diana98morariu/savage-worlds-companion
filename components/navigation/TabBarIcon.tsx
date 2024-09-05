a74594343ee8e44d6bf915f192c0f6d6e9a42cc4
Diana Morariu <32464376+diana98morariu@users.noreply.github.com>
2024-09-05 09:19:16 +0200

    changes small styling on tabs

--- components/navigation/TabBarIcon.tsx
+++ components/navigation/TabBarIcon.tsx
@@ -1,9 +1,29 @@
 // You can explore the built-in icon families and icons on the web at https://icons.expo.fyi/
 
-import Ionicons from '@expo/vector-icons/Ionicons';
-import { type IconProps } from '@expo/vector-icons/build/createIconSet';
-import { type ComponentProps } from 'react';
+import Ionicons from "@expo/vector-icons/Ionicons";
+import { type IconProps } from "@expo/vector-icons/build/createIconSet";
+import { type ComponentProps } from "react";
+import { StyleSheet, Dimensions, Platform } from "react-native";
 
-export function TabBarIcon({ style, ...rest }: IconProps<ComponentProps<typeof Ionicons>['name']>) {
-  return <Ionicons size={28} style={[{ marginBottom: -3 }, style]} {...rest} />;
+const { width, height } = Dimensions.get("window");
+
+export function TabBarIcon({
+  style,
+  ...rest
+}: IconProps<ComponentProps<typeof Ionicons>["name"]>) {
+  return <Ionicons size={28} style={[styles.iconContainer, style]} {...rest} />;
 }
+
+const styles = StyleSheet.create({
+  iconContainer: {
+    marginLeft: 0,
+    // Apply styles based on platform
+    // ...(Platform.OS === "ios" && {
+    //   marginRight: 5,
+    // }),
+    // Apply styles based on screen width
+    ...(width > 500 && {
+      marginLeft: -2,
+    }),
+  },
+});
